1abc40a3b6ff895b875f7df6ee655f9579c9ed29
yusufkocaturk <73437010+yusufkocaturk@users.noreply.github.com>
2021-05-15 16:54:38 +0300

    commit message - 2

--- frontend.h
+++ frontend.h
@@ -4,16 +4,68 @@
 #define BETA_BANK_FRONTEND_H
 
 
-void bakiyeSorgulama(){}
+void bakiyeSorgulama() {}
 
-void sifreDegistirme(){}
+void sifreDegistirme() {
+    char passwordCurrent[] = "123456";
+    char response[] = "";
+    int result;
+    printf("\nSIFRE DEGISTIRME");
+    printf("\nVAR OLAN SIFRENIZI GIRINIZ:\n");
+    scanf("%s", response);
+    result = strcmp(response, passwordCurrent);
 
-void ibanGoruntuleme(){}
+    if (result == 0) {
+        char passwordNew[] = "";
+        printf("\nYENI SIFRENIZI GIRINIZ:\n");
+        scanf(" %s", passwordNew);
+        printf("YENI SIFRENIZ: %s", passwordNew);
+    }
 
-void hesapOzeti(){}
+}
+
+void ibanGoruntuleme() {
+    char iban[] = "1234-5678-9101-1121";
+    printf("\nIBAN GORUNTULE\n");
+    printf("IBAN: %s", iban);
+}
+
+void hesapOzeti() {
+    printf("\nHESAP OZETI");
+}
 
 void betaKart_Menu() {
-    printf("5 - BETAKART MENUSU\n");
+    char response;
+
+    printf("BETAKART MENUSU\n");
+    printf("1 - HESAP OZETI\n");
+    printf("2 - IBAN GORUNTULE\n");
+    printf("3 - SIFRE DEGISTIR\n");
+    printf("4 - BAKIYE SORGULA\n");
+
+
+    printf("Menu numarasini giriniz: ");
+    scanf(" %c", &response);
+
+    switch (response) {
+        case '1':
+            hesapOzeti();
+            break;
+        case '2':
+            ibanGoruntuleme();
+            break;
+        case '3':
+            sifreDegistirme();
+            break;
+        case '4':
+            bakiyeSorgulama();
+            break;
+
+        default:
+            printf("\nHatali secim yaptiniz\n\n");
+            betaKart_Menu();
+            break;
+    }
 }
 
 void hisseSenediHesabi();
@@ -94,11 +146,48 @@ void anamenu() {
     }
 }
 
-void sinavOdemeleri_Menu();
+void sinavOdemeleri_Menu() {}
+
+void girisYap() {
+    char password[] = "123456";
+    char response[6];
+    int result;
+    printf("GIRIS YAP\n\n");
+    printf("HESABINIZA AIT SIFREYI GIRINIZ:");
+
+    //gets(response);
+    scanf(" %s", response);
+    printf("şifre : %s", response);
+
+    result = strcmp(response, password);
+//    printf("%d", result);
+
+    if (result == 0) {
+        anamenu();
+    } else {
+        printf("\nHATALI SIFRE GIRDINIZ TEKRAR DENEYIN\n");
+        girisYap();
+    }
+}
 
 void applicationStart() {
 
-    anamenu();
+    char response;
+    printf("BETABANK'A HOS GELDINIZ\n");
+    printf("1 - GIRIS YAP\n");
+    printf("2 - KARTSIZ ISLEMLER(SINAV ODEMELERI)\n");
+    scanf(" %c", &response);
+    switch (response) {
+        case '1':
+            girisYap();
+            break;
+        case '2':
+            sinavOdemeleri_Menu();
+            break;
+        default:
+            applicationStart();
+            break;
+    }
 }
 
 
